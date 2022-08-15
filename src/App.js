c52bdd7801e79def0ed52014d9c37f6c437fdce0
harisalkarni <haris.alkarni@gmail.com>
2022-08-15 18:00:09 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
+import Web3Modal from 'web3modal'
 import './App.css';
+import {ethers} from 'ethers'
+import WalletConnectProvider from "@walletconnect/web3-provider";
+
+
+const providerOptions = {
+  binancechainwallet: {
+    package: true
+  }
+  // walletconnect: {
+  //   package: WalletConnectProvider, // required
+  //   options: {
+  //     infuraId: {3: 'https://ropsten.infura.io/v3/fefnefnosfe'} // required
+  //   }
+  // }
+}
 
 function App() {
+
+  async function connectWallet() {
+    try {
+      let web3Modal = new Web3Modal({
+        cacheProvider: false,
+        providerOptions
+      })
+      const web3ModalInstance = await web3Modal.connect();
+      const web3ModalProvider = new ethers.providers.Web3Provider(web3ModalInstance)
+      console.log(web3ModalProvider)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Web3Modal Connect Wallet</h1>
+        <button onClick={connectWallet}>Connect</button>
       </header>
     </div>
   );
